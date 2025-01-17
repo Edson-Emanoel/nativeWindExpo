3437cf1583493848092fcef515edc4151cbed21a
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-01-17 20:53:10 -0300

    Tentei configurar mas não deu certo

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+export default function Home(){
+    return(
+        <View className='bg-red-700 w-10 h-10'>
+            <h1>Olá Mundooo!</h1>
+        </View>
+    )
+}
